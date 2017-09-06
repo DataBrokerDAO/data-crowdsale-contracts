1c8c01876a0ac971c577417dea909db97f70b315
Roderik van der Veer <roderik@settlemint.com>
2017-09-06 22:17:12 +0200

    feat: deploy to rinkeby

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
+const Promise = require('bluebird');
+
 const MiniMeTokenFactory = artifacts.require('MiniMeTokenFactory');
 const EarlyTokenSale = artifacts.require('EarlyTokenSale');
 const MultiSigWallet = artifacts.require('MultiSigWallet');
 const DataBrokerDaoToken = artifacts.require('DataBrokerDaoToken');
 
-async function performMigration(deployer) {
+async function performMigration(deployer, network) {
   // Deploy the MiniMeTokenFactory, this is the factory contract that can create clones of the token
   await deployer.deploy(MiniMeTokenFactory);
 
@@ -21,24 +23,46 @@ async function performMigration(deployer) {
   // Deploy the actual DataBrokerDaoToken, the controller of the token is the one deploying. (Roderik)
   await deployer.deploy(DataBrokerDaoToken, MiniMeTokenFactory.address);
 
-  // Deploy the Early Token Sale, again owned by the one deploying (Roderik)
-  await deployer.deploy(
-    EarlyTokenSale,
-    1505750400, // 09/18/2017 @ 4:00pm (UTC) = 5:00pm (CET)
-    1508169600, // 10/16/2017 @ 4:00pm (UTC) = 5:00pm (CET)
-    MultiSigWallet.address,
-    DataBrokerDaoToken.address
-  );
+  if (network === 'mainnet') {
+    // Deploy the Early Token Sale, again owned by the one deploying (Roderik)
+    await deployer.deploy(
+      EarlyTokenSale,
+      1505746800, // 09/18/2017 @ 5:00pm (CET)
+      1507647600, // 10/16/2017 @ 5:00pm (CET)
+      MultiSigWallet.address,
+      DataBrokerDaoToken.address
+    );
+  } else {
+    const getBlock = Promise.promisify(web3.eth.getBlock);
+    const { timestamp } = await getBlock('latest');
+    // Deploy the Early Token Sale, again owned by the one deploying (Roderik)
+    await deployer.deploy(
+      EarlyTokenSale,
+      timestamp - 3600,
+      timestamp + 2592000,
+      MultiSigWallet.address,
+      DataBrokerDaoToken.address
+    );
+  }
 
   // Set the controller of the token to the early token sale
   const DeployedDataBrokerDaoToken = await DataBrokerDaoToken.deployed();
   DeployedDataBrokerDaoToken.changeController(EarlyTokenSale.address);
+
+  const sendTransaction = Promise.promisify(web3.eth.sendTransaction);
+
+  await sendTransaction({
+    from: accounts[0],
+    to: sale.address,
+    value: web3.toWei(0.1, 'ether'),
+    gas: 200000,
+  });
 }
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network) {
   deployer
     .then(function() {
-      return performMigration(deployer);
+      return performMigration(deployer, network);
     })
     .catch(error => {
       console.log(error);
